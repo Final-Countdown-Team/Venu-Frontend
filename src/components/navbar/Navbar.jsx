407cd6bb2196a0c9afb0ad633e8da84d713f9c91
Hammed Waheed <worldwid01@gmail.com>
2022-11-30 04:24:57 +0100

    create navbar components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Sidebar from "../navbar/Sidebar";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+	const [showSidebar, setShowSidebar] = React.useState(false);
+	const location = useLocation();
+
+	const links = [
+		{
+			name: "Home",
+            path: "/",
+            icon: faHome
+		},
+		{
+			name: "artists",
+			path: "/artists",
+		},
+		{
+			name: "Venues",
+			path: "/venues",
+		},
+		{
+			name: "about",
+			path: "/about",
+		},
+		{
+			name: "login",
+			path: "/login",
+		},
+		{
+			name: "sign up",
+			path: "/signup",
+		},
+	];
+
+	function closeSidebar() {
+		setShowSidebar(false);
+	}
+
+	return (
+		<>
+			<div className="navbar container">
+				<Link to="/" className="logo">
+					<img
+						src="../../public/img/projectLogo.png"
+						alt=""
+						width="50"
+						height="50"
+						boarder-radius="50"
+					/>
+				</Link>
+				<div className="nav-links">
+					{links.map((link) => (
+						<Link
+							className={location.pathname === link.path ? "active" : ""}
+							to={link.path}
+							key={link.name}
+						>
+							{link.name}
+						</Link>
+					))}
+				</div>
+				<div
+					onClick={() => setShowSidebar(true)}
+					className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+				>
+					<div className="bar"></div>
+					<div className="bar"></div>
+					<div className="bar"></div>
+				</div>
+			</div>
+			{showSidebar && <Sidebar close={closeSidebar} links={links} />}
+		</>
+	);
+}
