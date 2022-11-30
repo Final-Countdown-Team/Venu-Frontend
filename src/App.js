f6432001449ec28ea42c5c6d5e974a379d2d3436
druckmax <max_sommerfeld@web.de>
2022-11-30 10:36:32 +0100

    Updates navbar, Fixes scss

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
 import Signup from "./components/pages/Signup";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="wrapper">
         <Routes>
+          <Route path="/" />
           <Route path="/venues/signup" element={<Signup type={"venues"} />} />
           <Route path="/artists/signup" element={<Signup type={"artists"} />} />
         </Routes>
--- src/components/navbar/Sidebar.jsx
+++ src/components/navbar/Sidebar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Sidebar(links, close) {
-	const location = useLocation();
+export default function Sidebar({ links, close }) {
+  const location = useLocation();
 
-	return (
-		<div className="sidebar" onClick={close}>
-			{links.map((link) => (
-				<Link
-					to={link.path}
-					className={
-						location.pathname === link.path
-							? "sidebar-link active"
-							: "sidebar-link"
-					}
-					key={link.name}
-				>
-					<FontAwesomeIcon icon={link.icon} />
-					{link.name}
-				</Link>
-			))}
-		</div>
-	);
+  return (
+    <div className="sidebar" onClick={close}>
+      {links.map((link) => (
+        <Link
+          to={link.path}
+          className={
+            location.pathname === link.path
+              ? "sidebar-link active"
+              : "sidebar-link"
+          }
+          key={link.name}
+        >
+          {/* <FontAwesomeIcon icon={link.icon} /> */}
+          {link.name}
+        </Link>
+      ))}
+    </div>
+  );
 }
