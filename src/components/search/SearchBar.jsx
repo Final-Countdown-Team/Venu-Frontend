4592a9ed83fee88bf745ea800a58f9e375384d42
Hammed Waheed <worldwid01@gmail.com>
2022-12-01 12:46:30 +0100

    search component

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,9 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export default function SearchBar() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function SearchBar(props) {
+	const { onSearch } = props;
+
+	const [searchText, setSearchText] = useState("");
+
+	const handleSearch = (e) => {
+		const text = e.target.value;
+		setSearchText(text);
+	};
+
+	const handleSubmit = (e) => {
+		if (e.key === "Enter") {
+			onSearch(searchText);
+		}
+	};
+
+	return (
+		<div className="saerchBar">
+			<input
+				onChange={handleSearch}
+				onKeyPress={handleSubmit}
+				type="search"
+				value={searchText}
+				placeholder="Search for artist"
+			/>
+		</div>
+	);
 }
