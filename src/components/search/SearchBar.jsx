5ae456f0dc8bcf1de50c6dcbb351d279f55aca3e
Hammed Waheed <worldwid01@gmail.com>
2022-12-01 13:29:53 +0100

    search bar

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// import "./_SearchBar.scss";
 
 export default function SearchBar(props) {
 	const { onSearch } = props;
@@ -14,7 +15,18 @@ export default function SearchBar(props) {
 		if (e.key === "Enter") {
 			onSearch(searchText);
 		}
-	};
+    };
+    
+    const inputStyle = {
+			width: "40%",
+			height: "40px",
+			borderRadius: "20px",
+			// border: "1px solid #ccc",
+			position: "relative",
+			zIndex: "1",
+			padding: "0 20px",
+			marginTop: "20px",
+		};
 
 	return (
 		<div className="searchBar">
@@ -24,6 +36,7 @@ export default function SearchBar(props) {
 				type="text"
 				value={searchText}
 				placeholder="Search for artist"
+				style={inputStyle}
 			/>
 		</div>
 	);
