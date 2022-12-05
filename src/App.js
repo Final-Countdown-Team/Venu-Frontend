80ad32e583aa1dab23fc9155e4e06da737e930bd
moumita2022 <moumitachakr@googlemail.com>
2022-12-05 11:23:43 +0100

    footer done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Footer from "./components/footer/Footer";
 import Signup from "./components/pages/Signup";
 
 function App() {
@@ -11,9 +12,18 @@ function App() {
           <Route path="/venues/signup" element={<Signup type={"venues"} />} />
           <Route path="/artists/signup" element={<Signup type={"artists"} />} />
         </Routes>
+        
+        <Footer/>
       </div>
+      
     </Router>
+
   );
 }
 
 export default App;
+{/* <div className="page-container">
+          <div className="content-wrap"> 
+          </div>
+        </div>*/}
+       
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./_Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="footer-content">
+        <p >&copy;{new Date().getFullYear()}</p>
+        <p >Impressum</p>
+      </div>
+    </div>
+  );
+}
