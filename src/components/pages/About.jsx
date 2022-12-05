466770a6bd0ffcf9c66746fc6e7ecbdfce3969cc
Neetu <upneetneetu@gmail.com>
2022-12-05 14:11:30 +0100

    created about page

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function About() {
+    return (
+      <div className="about-desc">
+        <h3>About Us</h3>
+        <p>
+          Venu is an innovative and effortless app that makes music happen. We
+          connect musicians and venues, allowing you, the artist to plan your
+          upcoming tour in the most uncomplicated way possible. No more juggling
+          bookers and promoters. We’re cutting out the middlemen, and putting
+          the creative freedom back in your hands. We’re also proud to be
+          supporting local venues and beloved independent businesses by giving
+          them the opportunity to find and book new and exciting entertainers to
+          present. We love music, and we’re chuffed to help make the magic
+          happen! Just click, book, and get back to band practice! Venu. When
+          you wanna play!
+        </p>
+      </div>
+    );
+    
+}
+
+export default About;
