460961cbd6d005244ae6adfc742070415396f2c2
Neetu <upneetneetu@gmail.com>
2022-12-06 13:46:33 +0100

    props added

--- src/components/cards/PreviewCard.jsx
+++ src/components/cards/PreviewCard.jsx
@@ -2,23 +2,19 @@ import React from 'react';
 import "./_Preview-card.scss";
 
 
-export default function PreviewCard() {
+export default function PreviewCard(props) {
   return (
     <div class="card-container">
       <div class="float-layout">
         <div class="card-image">
-          <img
-            src="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcTYiQcFe-WmL2c9zefm1O1aDnRzz5QuEUK06F-bYAQdALalZjlGiCZkKp9GvV3crsSaJtofK-xziOe7hso"
-            alt="logo"
+          <img src={props.img} alt="profile pic"
           ></img>
           <div class="card">
-            <div class="card-title">Title</div>
+            <div class="card-title">{props.name}</div>
             <div class="card-desc">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              ullamcorper mollis tempus. Mauris eu maximus lectus, eu auctor
-              justo. Aenean porta purus vel commodo consequat.
+              {props.description}
             </div>
-            <button className='button-secondary'>Click me!</button>
+            <button className='button-secondary'>Visit profile</button>
           </div>
         </div>
       </div>
