c5f479426daf7bef985db8c9b1e6caf4523f09b8
Upneet Neetu Bains <92793615+NeetrontheCoder@users.noreply.github.com>
2022-12-07 14:34:04 +0100

    Reusable card (#28)
    * started reusable-card styling
    
    * styling changes
    
    * reusable-card styling complete
    
    * updated styling
    
    * preview card updated with scss nesting.
    
    * added react icon
    
    Co-authored-by: Max <max_sommerfeld@web.de>

--- src/components/cards/PreviewCard.jsx
+++ src/components/cards/PreviewCard.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
+import { CardButton } from "../buttons/CardButton";
 import "./_Preview-card.scss";
-
 
 export default function PreviewCard(props) {
   return (
     <div class="card-container">
       <div class="float-layout">
         <div class="card-image">
-          <img src={props.img} alt="profile pic"
-          ></img>
+          <img src={props.img} alt="profile pic"></img>
           <div class="card">
             <div class="card-title">{props.name}</div>
-            <div class="card-desc">
-              {props.description}
-            </div>
-            <button className='button-secondary'>Visit profile</button>
+            <div class="card-desc">{props.description}</div>
+            <div className="profile-button"><CardButton /></div>
           </div>
         </div>
       </div>
