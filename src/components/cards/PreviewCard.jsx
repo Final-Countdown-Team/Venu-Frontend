a93e015493817346ed592d563dce4b5b3422aa0b
Neetu <upneetneetu@gmail.com>
2022-12-07 11:04:32 +0100

    started reusable-card styling

--- src/components/cards/PreviewCard.jsx
+++ src/components/cards/PreviewCard.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./_Preview-card.scss";
-
 
 export default function PreviewCard(props) {
   return (
     <div class="card-container">
       <div class="float-layout">
         <div class="card-image">
-          <img src={props.img} alt="profile pic"
-          ></img>
+          <img src={props.img} alt="profile pic"></img>
           <div class="card">
             <div class="card-title">{props.name}</div>
-            <div class="card-desc">
-              {props.description}
-            </div>
-            <button className='button-secondary'>Visit profile</button>
+            <div class="card-desc">{props.description}</div>
           </div>
         </div>
       </div>
