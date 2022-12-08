0897ddc34743ff1b703cd34474ef1a7be48d6f7e
Neetu <upneetneetu@gmail.com>
2022-12-08 14:29:23 +0100

    created error page

---
+++ src/components/pages/FourOhFour.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ErrorPage from '../../img/404.gif'
+import "./_FourOhFour.scss";
+
+
+export const FourOhFour = () => {
+  return (
+    <div className='page-error'>
+      <img class= 'error' src={ErrorPage} alt="404 error" /></div>
+
+
+  )
+}
