de839c980424ce9fe25e3ec4a97c1adb208ad286
druckmax <max_sommerfeld@web.de>
2022-12-13 09:37:48 +0100

    Sets up layout signup/login page

--- src/components/pages/SignupLogin.jsx
+++ src/components/pages/SignupLogin.jsx
@@ -3,47 +3,22 @@ import ReuseButton from "../../components/buttons/Reusable_BB";
 import ArrowBack from "../utils/ArrowBack";
 import "./_SignupLogin.scss";
 
-
 function SignupLogin({ userType }) {
   return (
     <div className="signup-container">
       <ArrowBack userType={userType} className="arrow-back-login" />
-      <div className="venues-container">
+      <div className="userType-container">
         <h2>Venues</h2>
         <div className="button-container">
-          <div className="button-reuse--venue">
-            <ReuseButton
-              text="Sign Up"
-              purpose="link to signup"
-              userType="venues"
-            ></ReuseButton>
-          </div>
-          <div className="button-reuse--venue">
-            <ReuseButton
-              text="Log In"
-              purpose="link to login"
-              userType="venues"
-            ></ReuseButton>
-          </div>
+          <ReuseButton text="Sign Up" purpose="link to signup" userType="venues"></ReuseButton>
+          <ReuseButton text="Log In" purpose="link to login" userType="venues"></ReuseButton>
         </div>
       </div>
-      <div className="artist-container">
+      <div className="userType-container">
         <h2>Artists</h2>
         <div className="button-container">
-          <div className="button-reuse--artist">
-            <ReuseButton
-              text="Sign Up"
-              purpose="link to signup"
-              userType="artists"
-            ></ReuseButton>
-          </div>
-          <div className="button-reuse--artist">
-            <ReuseButton
-              text="Log In"
-              purpose="link to login"
-              userType="artists"
-            ></ReuseButton>
-          </div>
+          <ReuseButton text="Sign Up" purpose="link to signup" userType="artists"></ReuseButton>
+          <ReuseButton text="Log In" purpose="link to login" userType="artists"></ReuseButton>
         </div>
       </div>
     </div>
