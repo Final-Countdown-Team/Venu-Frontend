b493f937481bfe2cfa4bc48489ed20096033aa46
Neetu <upneetneetu@gmail.com>
2022-12-14 13:24:39 +0100

    cleaned up code

--- src/components/searchbar/CustomDropdown.jsx
+++ src/components/searchbar/CustomDropdown.jsx
@@ -1,4 +1,3 @@
-//  use the context userType state, which is set up in the Maincontext to match the color scheme for the usertype in the dropdown of the searchbar. The highlight should be the respective color code of the artist or venue.
 
 import React, { useContext } from 'react';
 import { MainContext } from '../../contexts/MainContext';
