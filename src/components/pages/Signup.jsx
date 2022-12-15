7589d4a01dbcc0cddf1b3baa4506a4bc72c4e93b
druckmax <max_sommerfeld@web.de>
2022-12-15 14:14:16 +0100

    Styles ScrollUpButton
    - Issues: Cannot set to position:fixed because of moton.div
      - Placed button on bottom of signup form together with back arrow
      - deactivated visibility functionality in button
    - Adds general color and bgc classes for artists and venues in _Utilities for later refactoring

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -9,23 +9,26 @@ import ScrollUpButton from "../buttons/ScrollUpButton";
 
 function Signup({ userType }) {
   return (
-    <motion.div
-      variants={containerVariantY}
-      initial="exit"
-      animate="visible"
-      exit="hidden"
-      className="signup-page"
-    >
-      <div className="signup-heading">
-        {userType === "venues" ? <h1>Venues</h1> : <h1>Artists</h1>}
-        <h2>Sign Up</h2>
-      </div>
-      <SignupForm userType={userType} />
-      <div className="arrow-wrapper">
-        <ArrowBack userType={userType} />
-      </div>
-      <ScrollUpButton />
-    </motion.div>
+    <>
+      <motion.div
+        variants={containerVariantY}
+        initial="exit"
+        animate="visible"
+        exit="hidden"
+      >
+        <div className="signup-page">
+          <div className="signup-heading">
+            {userType === "venues" ? <h1>Venues</h1> : <h1>Artists</h1>}
+            <h2>Sign Up</h2>
+          </div>
+          <SignupForm userType={userType} />
+          <div className="arrow-wrapper">
+            <ArrowBack userType={userType} />
+            <ScrollUpButton userType={userType} />
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 }
 
