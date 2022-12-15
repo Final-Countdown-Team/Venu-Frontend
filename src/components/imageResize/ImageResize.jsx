2b6578f16b4ba0bbb5b94543e57835f7ad5f6cab
Hammed Waheed <worldwid01@gmail.com>
2022-12-15 14:20:42 +0100

    image-resize

--- src/components/imageResize/ImageResize.jsx
+++ src/components/imageResize/ImageResize.jsx
@@ -1,55 +1,50 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Resizer from "react-image-file-resizer";
-import prettyBytes from "pretty-bytes";
+
 
 import React from "react";
 
-function ImageResize({ title, description, file }) {
-	const [image, setImage] = useState();
+function ImageResize(props) {
 
-	const handleImage = (image) => {
-		setImage(image);
-	};
+	const [newImage, setnewImage] = useState("");
+	
+	const resizeFile = (file) =>
+		new Promise((resolve) => {
+			Resizer.imageFileResizer(
+				file,
+				200,
+				300,
+				"JPEG",
+				100,
+				0,
+				(uri) => {
+					console.log(uri);
+					setnewImage(uri);
+				},
+				"base64"
+			);
+		});
 
-	useEffect(() => {
-		if (file) {
-			console.log("File received", file);
-			try {
-				Resizer.imageFileResizer(
-					file,
-					230,
-					120,
-					"JPEG",
-					50,
-					0,
-					(blob) => {
-						handleImage({
-							url: URL.createObjectURL(blob),
-							size: blob.size,
-							type: blob.type,
-							name: file.name,
-						});
-					},
-					"blob",
-					50,
-					50
-				);
-			} catch (error) {
-				console.log(error);
-			}
+	const onChange = async (event) => {
+		try {
+			const file = event.target.files[0];
+			const image = await resizeFile(file);
+			console.log(image);
+		} catch (err) {
+			console.log(err);
 		}
-	}, [file]);
-
-	if (!image) return file;
+	};
+	
 
 	return (
-		<div>
-			<>
-				<h1>{title}</h1>
-				<p>{description}</p>
-				<figure>{`${prettyBytes(image.size)} ${image.type}`} </figure>
-				<img src={image.url} alt={image.name} style={{ maxWidth: "100%" }} />
-			</>
+		<div className="App">
+			<input
+				type="file"
+				onChange={(e) => {
+					onChange(e);
+				}}
+			/>
+			<img src={newImage} alt="" />
 		</div>
 	);
 }
